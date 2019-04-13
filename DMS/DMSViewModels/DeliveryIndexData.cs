e9879926b2ede36c01e167f23e7690c47c685cc1
tanki97 <1107943@students.wits.ac.za>
2019-04-13 23:34:04 +0200

    l

---
+++ DMS/DMSViewModels/DeliveryIndexData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DMS.Models;
+
+namespace DMS.DMSViewModels
+{
+    public class DeliveryIndexData
+    {
+        public IEnumerable<Stock> Stock { get; set; }
+        public IEnumerable<Shop> Shops { get; set; }
+        public IEnumerable<Delivery> Deliveries { get; set; }
+    }
+}
