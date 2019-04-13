7420985baa6c2ac20de69ac6276e722e53599aa4
Filwe <mokoenar6@gmail.com>
2019-04-13 23:52:52 +0200

    Models + working

---
+++ DMS/DMS/Data/DMSContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DMS.Models;
+
+namespace DMS.Models
+{
+    public class DMSContext : DbContext
+    {
+        public DMSContext (DbContextOptions<DMSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DMS.Models.Delivery> Delivery { get; set; }
+
+        public DbSet<DMS.Models.Shop> Shop { get; set; }
+
+        public DbSet<DMS.Models.Stock> Stock { get; set; }
+    }
+}
---
+++ DMS/DMS/Models/Delivery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.Models
+{
+    public class Delivery
+    {
+        public int DeliveryID { get; set; }
+        public int ShopID { get; set; }
+        public int Stock { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DeliveryDate { get; set; }
+        public int Quantity { get; set; }
+    }
+}
