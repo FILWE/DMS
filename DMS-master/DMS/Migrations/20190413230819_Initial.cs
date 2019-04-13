eed71c618ee86927e3203c70f6ffcf20adfa5fbf
Filwe <mokoenar6@gmail.com>
2019-04-14 01:52:52 +0200

    Data annotations done

---
+++ DMS-master/DMS/Migrations/20190413230819_Initial.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DMS.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Delivery",
+                columns: table => new
+                {
+                    DeliveryID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ShopID = table.Column<int>(nullable: false),
+                    StockID = table.Column<int>(nullable: false),
+                    DeliveryDate = table.Column<DateTime>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Delivery", x => x.DeliveryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shop",
+                columns: table => new
+                {
+                    ShopID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Contact = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shop", x => x.ShopID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stock",
+                columns: table => new
+                {
+                    StockID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stock", x => x.StockID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Delivery");
+
+            migrationBuilder.DropTable(
+                name: "Shop");
+
+            migrationBuilder.DropTable(
+                name: "Stock");
+        }
+    }
+}
---
+++ DMS-master/DMS/Models/Delivery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+//using System.Web.Mvc;
+
+namespace DMS.Models
+{
+    public class Delivery
+    {
+        [ScaffoldColumn(false)]
+        public int DeliveryID { get; set; }
+
+        [DisplayName("Shop ID")]
+        public int ShopID { get; set; }
+
+        [DisplayName("Stock ID")]
+        public int Stock { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "The delivery date is required")]
+        public DateTime DeliveryDate { get; set; }
+
+        [Required(ErrorMessage = "The quantity is required")]
+        [Range(1, 2000)]
+        public int Quantity { get; set; }
+    }
+}
