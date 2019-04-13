4a9a5a633a352e369a4413fb6d03dd84f157dd96
Filwe <mokoenar6@gmail.com>
2019-04-13 22:58:20 +0200

    Models

---
+++ Latest/DMS/DMS/DMS/Models/Shop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.Models
+{
+    public class Shop
+    {
+        public int ShopID { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+    }
+}
