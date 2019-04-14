594a5bfc1a8dcd7a19bd90c1a50321d22055cf8e
Filwe <mokoenar6@gmail.com>
2019-04-14 22:23:44 +0200

    Views

--- DMS-master/DMS/Models/Stock.cs
+++ DMS-master/DMS/Models/Stock.cs
@@ -1,39 +1,6 @@
-﻿
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -41,10 +8,27 @@ namespace DMS.Models
 {
     public class Stock
     {
+        [ScaffoldColumn(false)]
         public int StockID { get; set; }
+
+        [Required(ErrorMessage = "The description is required")]
+        [Range(1, 2000)]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "The quantity is required")]
+        [Range(1, 1000000)]
         public double Price { get; set; }
 
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "The manufacturing date is required")]
+        public DateTime ManufacturingDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "The stock-in date is required")]
+        public DateTime StockInDate { get; set; }
+
+
         public ICollection<Delivery> Deliveries { get; set; }
+       // public object Delivery { get; internal set; }
     }
 }
